883cdb349b9d7bdf2c109faaa3b83a22a343f2db
Sebastian Vogel <sebastian@magnetico.dev>
2021-05-22 17:41:11 -0300

    create header

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,34 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import React from 'react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <Link to="/">{siteTitle}</Link>
-  </header>
-)
+const Header = ({}) => {
+  return (
+    <Flex as="nav" align="center" justify="space-between" wrap="wrap" bg="primary" color="white">
+      <Box align="center" flex={0.3}>
+        <Text letterSpacing={'tighter'} bg="gray" flex={1} pt={6} pb={6}>
+          Graltec
+        </Text>
+      </Box>
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+      <Box flex={0.7} p={6} display="flex" justifyContent="space-around">
+        <Text flex={1} align="center">
+          Equipos
+        </Text>
+        <Text flex={1} align="center">
+          Cortinas
+        </Text>
+        <Text flex={1} align="center">
+          Servicios
+        </Text>
+        <Text flex={1} align="center">
+          Accesorios
+        </Text>
+        <Text flex={1} align="center">
+          Contacto
+        </Text>
+      </Box>
+    </Flex>
+  )
 }
 
 export default Header
