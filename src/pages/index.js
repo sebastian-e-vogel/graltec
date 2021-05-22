af6fdf03056fe50352620dba7876d797bca08b76
Sebastian Vogel <sebastian@magnetico.dev>
2021-05-22 17:48:06 -0300

    generate right blue lateral

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 import Seo from '../components/seo'
+import { Box, Flex, Text } from '@chakra-ui/react'
 
 const LandingPage = () => (
   <>
     <Seo title="Home" />
+    <Flex justify="space-between">
+      <Box align="center"></Box>
+
+      <Box flex={0.15} height="100px" bg="primary"></Box>
+    </Flex>
   </>
 )
 
